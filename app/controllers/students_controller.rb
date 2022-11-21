54b18c6bb8e81b52baaf224121d41a5f94d8e37a
Alexis Torosina <alexistorosina1@gmail.com>
2022-11-21 17:44:39 -0500

    commit tree 8d1bf6d99ca4e37979300d25b3a9bd529ebfa46c

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,34 @@
 class StudentsController < ApplicationController
+  # check if theres a query param called "name"
+  # only want to get students that have a matching first or last name // case insensitive
+  # return the first or last name if query matches 
 
+  # def index
+  #   if students = Student.all
+  #   render json: students 
+  #   else 
+  #     students = Student.find_by(params[:name])
+  #   end
+  # end
+
+  # def index
+  #   if students = Student.where("lower(last_name) = ? OR lower(first_name) =?", params[:name].downcase, params[:name].downcase)
+  #   else
+  #     Student.all
+  #   end
+  # end
   def index
-    students = Student.all
+    students = if params[:name]
+                 Student.where("lower(last_name) = ? OR lower(first_name) =?", params[:name].downcase, params[:name].downcase)
+               else
+                 Student.all
+               end    
+    render json: students
+  end
+
+
+  def show
+    students = Student.find(params[:id])
     render json: students
   end
 
